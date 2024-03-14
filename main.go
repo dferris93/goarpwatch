6e902baf7f6053159ab893c936c1377f26e92f91
Dan Ferris <dan@usrsbin.com>
2024-03-13 23:06:44 -0700

    alert on ip change as well as mac change

--- main.go
+++ main.go
@@ -13,25 +13,30 @@ type ArpReply struct {
 	Ip  net.IP
 }
 
-func checkMac(mac net.HardwareAddr, ip net.IP, MacMap map[string]string) int {
+func checkMac(mac net.HardwareAddr, ip net.IP, MacMap map[string]string, IPMap map[string]string ) (int, string) {
     if _, ok := MacMap[ip.String()]; !ok {
         MacMap[ip.String()] = mac.String()
+        IPMap[mac.String()] = ip.String()
         log.Printf("First time seeing %s at %s\n", ip, mac)
-        return 1
+        return 1, ""
     } else if MacMap[ip.String()] != mac.String() {
         log.Printf("ALERT! %s has a new MAC address: %s (was %s)\n", ip, mac, MacMap[ip.String()])
         MacMap[ip.String()] = mac.String()
-        return 2
+        return 2, MacMap[ip.String()]
+    } else if IPMap[mac.String()] != ip.String() {
+        log.Printf("ALERT! %s has a new IP address: %s (was %s)\n", mac, ip, IPMap[mac.String()])
+        IPMap[mac.String()] = ip.String()
+        return 2, IPMap[mac.String()]
     }
-    return 0
+    return 0, ""
 }
 
-func runAlert(alertCmd string, status int, reply ArpReply) error {
+func runAlert(alertCmd string, status int, reply ArpReply, old string) error {
     var args []string
     if status == 1 {
         args = []string{"new", reply.Ip.String(), reply.Mac.String()}
     } else if status == 2 {
-        args = []string{"changed", reply.Ip.String(), reply.Mac.String()}
+        args = []string{"changed", reply.Ip.String(), reply.Mac.String(), old}
     }
     cmd := exec.Command(alertCmd, args...)
     cmd.Stderr = &bytes.Buffer{}
@@ -53,6 +58,7 @@ func main() {
 	var iface string
 	var alertCmd string
     var MacMap = make(map[string]string)
+    var IPMap = make(map[string]string)
 
 	// Define flags
 	flag.StringVar(&iface, "interface", "eth0", "Specify the network interface to listen on")
@@ -69,11 +75,10 @@ func main() {
 	go capture(pcapHandle, packetChannel)
 	for {
 		arpReply := <-packetChannel
-		log.Printf("ARP reply from %s with MAC %s\n", arpReply.Ip, arpReply.Mac)
-        status := checkMac(arpReply.Mac, arpReply.Ip, MacMap)
-        if status == 1 || status == 2 {
+        status, old := checkMac(arpReply.Mac, arpReply.Ip, MacMap, IPMap)
+        if status != 0 {
             if alertCmd != "" {
-                err := runAlert(alertCmd, status, arpReply)
+                err := runAlert(alertCmd, status, arpReply, old)
                 if err != nil {
                     log.Printf("Error running alert command: %s\n", err)
                 }
